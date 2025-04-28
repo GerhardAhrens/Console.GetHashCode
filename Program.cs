bd8544de33d838c598cec2685f1e71e461beb591
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-04-28 10:31:13 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2025
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>28.04.2025 09:50:21</date>
+//
+// <summary>
+// Konsolen Applikation mit Menü
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace Console.GetHashCode
+{
+    using System;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. bisherige Standard Lösung");
+                Console.WriteLine("2. Klasse HashCode");
+                Console.WriteLine("3. Klasse HashCode, Generic Expression");
+                Console.WriteLine("X. Beenden");
+
+                Console.WriteLine("Wählen Sie einen Menüpunkt oder 'x' für beenden");
+                ConsoleKey key = Console.ReadKey(true).Key;
+                if (key == ConsoleKey.X)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    if (key == ConsoleKey.D1)
+                    {
+                        MenuPoint1();
+                    }
+                    else if (key == ConsoleKey.D2)
+                    {
+                        MenuPoint2();
+                    }
+                    else if (key == ConsoleKey.D3)
+                    {
+                        MenuPoint3();
+                    }
+                }
+            }
+            while (true);
+        }
+
+        private static void MenuPoint1()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
+            Console.ReadKey();
+        }
+
+        private static void MenuPoint2()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
+            Console.ReadKey();
+        }
+
+        private static void MenuPoint3()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mit einer beliebigen Taste zurück zum Menü!");
+            Console.ReadKey();
+        }
+    }
+}
